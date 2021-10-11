a734423b7ec796f845672cc540ff96fc71ffe5ff
Pavel Khmelevskiy <Khmelevskiy@irc.beltelecom.by>
2021-10-11 14:48:12 +0300

    first

---
+++ src/Page.vue
@@ -0,0 +1,14 @@
+﻿<template>
+    <div>Powered by...</div>
+    <div>...Vue {{ ver }}!</div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        data: () => ({
+            ver: 3
+        })
+    });
+</script>
